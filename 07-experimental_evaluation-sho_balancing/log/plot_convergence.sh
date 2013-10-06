e7d825041a79d50b78c5362829bb592e2d1bbd5e
Lucas Benedicic <lucas.benedicic@gmail.com>
2013-10-06 15:11:26 +0200

    Second draft, including all supervisor's corrections and content.-

---
+++ 07-experimental_evaluation-sho_balancing/log/plot_convergence.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+LOGS="$@"
+
+if [ $# -gt 1 ]; then
+    i=0
+    for l in ${LOGS}; do
+        i=$(( ${i} + 1 ))
+        # for SA only
+        # grep ':' ${l} | tr -s ' ' | cut -d' ' -f3 | tail -n+2 > /tmp/${i}
+
+        # for DASA only
+        #sort -nr ${l} > /tmp/${i}
+
+        grep score ${l} | tr -s ' ' | cut -d' ' -f4 | sort -nr > /tmp/${i}
+    done
+
+    rm -f /tmp/.conv
+    touch /tmp/.conv
+
+    for f in $( seq 1 ${i} ); do
+        cat /tmp/${f} | paste - /tmp/.conv > /tmp/.foo
+        cp /tmp/.foo /tmp/.conv
+    done
+
+    CMD="$( cat <<EOF
+import numpy as np
+
+t = np.genfromtxt (fname='/tmp/.conv')
+mi = t.min (axis=1)
+av = (t.sum (axis=1))/t.shape[1]
+ma = t.max (axis=1)
+
+for i in range (mi.size):
+    print ("%d\t%.2f\t%.2f\t%.2f" % (i+1, mi[i], av[i], ma[i]))
+
+EOF
+)"
+    python -c "${CMD}" > /tmp/.foo
+
+    CMD="$( cat <<EOF
+set terminal postscript eps enhanced monochrome font "Helvetica,20";
+set output "algorithm_convergence.eps";
+set grid;
+set format x "%.1e";
+set xtics font ",14";
+set xtics 20000;
+set xlabel 'Number of evaluations';
+set yrange [2280000.00:2460000.00];
+set format y "%.2e";
+set ytics 40000;
+set ytics font ",14";
+set ylabel 'Objective-function value';
+plot '/tmp/.foo' using 1:2 with lines lw 2 title 'Best', \
+     '/tmp/.foo' using 1:3 with lines lw 2 title 'Mean', \
+     '/tmp/.foo' using 1:4 with lines lw 2 title 'Worst';
+EOF
+)"
+    gnuplot -p -e "${CMD}"
+else
+    echo "Usage: $0 [log files ...]"
+    echo "Creates the convergence plot from the given log files.-"
+    exit -1
+fi
+
